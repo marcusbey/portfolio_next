5dbf70410c8afec58e5fa42fdda2c7d78d58ebce
Romain BOBOE <rboboe@gmail.com>
2024-10-28 12:10:17 -0400

    Adding datafast script file

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,4 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Head, Html, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
@@ -7,7 +7,18 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+        <script
+          src="https://nownownow.io/nownownow-widget-bundle.js"
+          data-user-id="your-user-id"
+          data-token="generated-token"
+        ></script>
+        <script
+          defer
+          data-website-id="671fb5ee22bbce1bee81d68c"
+          data-domain="romainboboe.com"
+          src="https://datafa.st/js/script.js"
+        ></script>
       </body>
     </Html>
-  )
+  );
 }
